8b28f67fd1e78b754eadf5c1f2530a57ea290fc6
binary <binary.weng@idreamsky.com>
2023-05-23 13:27:52 +0800

    Encode JPEG and GIF in isolate

--- lib/src/add_watermark_to_image.dart
+++ lib/src/add_watermark_to_image.dart
@@ -1,8 +1,22 @@
+import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_watermarked_image/src/watermark_config.dart';
 import 'package:image/image.dart' as img;
+
+class _DataPassToIsolate {
+  final int width;
+  final int height;
+  final List<int> frameDuration;
+  final int numFrames;
+  final List<ByteBuffer> data;
+  final int repetitionCount;
+
+  _DataPassToIsolate(this.width, this.height, this.frameDuration,
+      this.numFrames, this.data, this.repetitionCount);
+}
 
 Future<Uint8List> addWatermarkToImage(
   Uint8List srcData,
@@ -26,37 +40,34 @@ Future<Uint8List> addWatermarkToImage(
           config,
         )),
   );
-
   final width = frames.first.image.width;
   final height = frames.first.image.height;
 
   if (decoder is img.GifDecoder) {
-    final gifImage = img.Image.fromBytes(
-        numChannels: 4,
-        width: width,
-        height: height,
-        frameDuration: frames.first.duration.inMilliseconds,
-        bytes: watermarkedFrames.first!.buffer);
-    for (var i = 1; i < frames.length; i++) {
-      gifImage.addFrame(img.Image.fromBytes(
-          numChannels: 4,
-          width: width,
-          frameDuration: frames[i].duration.inMilliseconds,
-          height: height,
-          bytes: watermarkedFrames[i]!.buffer));
-    }
-    return img.encodeGif(gifImage, repeat: codec.repetitionCount);
+    return compute(
+        _encodeGif,
+        _DataPassToIsolate(
+          width,
+          height,
+          frames.map((e) => e.duration.inMilliseconds).toList(),
+          frames.length,
+          watermarkedFrames.map((e) => e!.buffer).toList(),
+          codec.repetitionCount,
+        ));
   } else if (isOutputFormatPng(decoder!)) {
     return watermarkedFrames.first!.buffer.asUint8List();
   } else {
-    return img.encodeJpg(
-        img.Image.fromBytes(
-          width: width,
-          height: height,
-          numChannels: 4,
-          bytes: watermarkedFrames.first!.buffer,
-        ),
-        quality: 80);
+    return compute(
+      _encodeJpeg,
+      _DataPassToIsolate(
+        width,
+        height,
+        [],
+        1,
+        [watermarkedFrames[0]!.buffer],
+        0,
+      ),
+    );
   }
 }
 
@@ -90,3 +101,36 @@ bool isOutputFormatPng(img.Decoder decoder) =>
     decoder is img.PngDecoder ||
     decoder is img.TiffDecoder ||
     decoder is img.WebPDecoder;
+
+FutureOr<Uint8List> _encodeGif(_DataPassToIsolate data) {
+  final gifImage = img.Image.fromBytes(
+      numChannels: 4,
+      width: data.width,
+      height: data.height,
+      frameDuration: data.frameDuration.first,
+      bytes: data.data.first);
+
+  for (var i = 1; i < data.numFrames; i++) {
+    gifImage.addFrame(
+      img.Image.fromBytes(
+        numChannels: 4,
+        width: data.width,
+        frameDuration: data.frameDuration[i],
+        height: data.height,
+        bytes: data.data[i],
+      ),
+    );
+  }
+  return img.encodeGif(gifImage, repeat: data.repetitionCount);
+}
+
+FutureOr<Uint8List> _encodeJpeg(_DataPassToIsolate data) {
+  return img.encodeJpg(
+      img.Image.fromBytes(
+        width: data.width,
+        height: data.height,
+        numChannels: 4,
+        bytes: data.data[0],
+      ),
+      quality: 80);
+}
